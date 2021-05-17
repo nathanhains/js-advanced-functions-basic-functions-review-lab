5b0308cf6b7d660c5f5befd8cc2b1fef511c29b9
nathan hains <nathanhains14@yahoo.com>
2021-05-17 19:34:54 -0400

    commit tree dc325310dce1025591bb9e48ea8b28e40bae9575

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(param = "special"){
+        return `You are ${flair}${param}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(num1, num2){return (num1+num2)},
+    subtract: function(num1, num2){return (num1-num2)},
+    multiply: function(num1, num2){return (num1*num2)},
+    divide: function(num1, num2){return (num1/num2)}
+}
+
+function actionApplyer(num, array){
+    array.forEach(x=> num = x(num))
+    return num
+    
+}
